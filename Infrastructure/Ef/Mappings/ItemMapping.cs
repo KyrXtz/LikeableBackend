3d834e8c59b693b2ed2deba466f67c7d3ea1b87c
KyrXtz <kiriakaras007@hotmail.com>
2023-01-26 23:42:04 +0200

    item ef mapping

--- Infrastructure/Ef/Mappings/ItemMapping.cs
+++ Infrastructure/Ef/Mappings/ItemMapping.cs
@@ -16,6 +16,34 @@
             builder.OwnsOne(p => p.Info, p =>
             {
                 p.Property(x => x.Description).HasColumnName("Description").HasMaxLength(3000);
+                p.Property(x => x.Title).HasColumnName("Title").HasMaxLength(100);
+            });
+
+            builder.OwnsOne(p => p.Comments, p =>
+            {
+                p.Property(x => x.CommentList).HasConversion(
+                    cmnt => string.Join(",", cmnt),
+                    cmnt => cmnt.Split(',', StringSplitOptions.RemoveEmptyEntries).ToList());
+            });
+
+            builder.OwnsOne(p => p.Images, p =>
+            {
+                p.Property(x => x.ImageList).HasConversion(
+                    img => string.Join(",", img),
+                    img => img.Split(',', StringSplitOptions.RemoveEmptyEntries).ToList());
+            });
+
+            builder.OwnsOne(p => p.Sound, p =>
+            {
+                p.Property(x => x.SoundPath).HasColumnName("SoundPath");
+            });
+
+            builder.OwnsOne(p => p.PrintDetails, p =>
+            {
+                p.Property(x => x.HandPainted).HasColumnName("HandPainted");
+                p.Property(x => x.Filament).HasColumnName("Filament");
+                p.Property(x => x.Height).HasColumnName("Height");
+                p.Property(x => x.Time).HasColumnName("Time");
             });
 
             builder.HasMany<UserLikedItem>()
